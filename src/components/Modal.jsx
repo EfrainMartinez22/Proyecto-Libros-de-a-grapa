2ef5436664890ec9cc5503ad0a6a8c7b1bdfdfa0
LuisDani07 <tics270118@gmail.com>
2022-11-03 10:34:39 -0700

    se crea el componente modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Modal() {
+  return (
+    <>
+      <div className='overlay'>
+        <div className="overlay-inner">
+            <button className='close'>close</button>
+            <div className="inner-box">
+                {/* <img src="./images/book" alt="" /> */}
+                <div className="info">
+                    <h1>React js by example- building modern web applications</h1>
+                    <h3>Luis carrillo</h3><br/>
+                    <h4>pakt publiching ltd<span>2016-02-23</span></h4><br/>
+                    <a href="#"><button>more</button></a>
+                </div>
+            </div>
+            <h4 className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam repudiandae, cumque hic assumenda accusamus soluta quos itaque, iste voluptate quibusdam culpa cupiditate reprehenderit tempora sapiente odio doloremque laudantium sunt sint nesciunt eaque debitis facilis aspernatur! Vitae voluptates et similique eligendi illum suscipit, ipsum minima, nihil quae placeat quidem, veritatis rem?</h4>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Modal
